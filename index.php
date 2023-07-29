3c85eff0f9becd07b9d640f199b9c9ed09c0681a
Suraj Sharma <67861900+surajssharma@users.noreply.github.com>
2023-07-29 20:39:06 +0530

    replace the same data

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-Testing page 1
+<html>
+
+  <head>
+    <title>Hello World!</title>
+    <style>
+      h1 {
+        text-align: center;
+      }
+      
+      h2 {
+        text-align: left;
+      }
+      
+      h3 {
+        text-align: right;
+      }
+</style>
+  </head>
+
+  <body>
+    <?php echo "<h1> Hello World! </h1>"; ?>
+    
+    <?php phpinfo(); ?>
+  </body>
+
+</html>
